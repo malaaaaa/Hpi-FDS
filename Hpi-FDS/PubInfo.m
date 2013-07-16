fe62d31e4d585efce4e75d98c9f5dc8513899ae7
mawp <mawp@github>
2013-07-16 12:50:29 +0800

    部署版本改为5.0

--- Hpi-FDS/PubInfo.m
+++ Hpi-FDS/PubInfo.m
@@ -26,14 +26,14 @@
 #import "TBXMLParser.h"
 @implementation PubInfo
 
-static NSString *version = @"1.4";
+static NSString *version = @"1.5";
 //测试环境
-static NSString *hostName =@"http://10.2.17.121";
-static NSString *port =@":82";
+//static NSString *hostName =@"http://10.2.17.121";
+//static NSString *port =@":82";
 
 //正式环境
-//static NSString *hostName =@"http://cds.hpi.com.cn";
-//static NSString *port =@"";
+static NSString *hostName =@"http://cds.hpi.com.cn";
+static NSString *port =@"";
 static NSString *autoUpdate;
 static NSString *baseUrl;
 static NSString *url;
